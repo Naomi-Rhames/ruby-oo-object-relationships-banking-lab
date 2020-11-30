a83750642f8ca1de4205d3b4bc76dd304c5b01f7
Naomi-Rhames <naomirhames2020@gmail.com>
2020-11-30 09:34:42 -0600

    commit tree 68547a6646216545a8d9675a940bc7d809f65240

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,11 @@
 class BankAccount
-
+    attr_reader :name, :balance, :status
+    def initialize(name, balance = 1000, status = "open")
+        @name = name
+        @balance = balance
+        @status = status
+    end
+    def deposit(take_out)
+        
+    end
 end
